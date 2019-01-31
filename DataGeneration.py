1224808b81781a5ae8a340d0ffe8bc97cd604e0a
Linus Lagergren <laggan93@gmail.com>
2019-01-31 21:10:58 +0100

    Made a simple class which generates and contains random two dimensional data from 2 classes

--- DataGeneration.py
+++ DataGeneration.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 class SimpleDataset:
@@ -7,7 +8,7 @@ class SimpleDataset:
         n = numberOfPoints
         labels1 = np.ones([numberOfPoints, 1])
         labels2 = 2*np.ones([numberOfPoints, 1])
-        self.labels = np.concatenate(labels1, labels2)
+        self.labels = np.concatenate([labels1, labels2])
 
         # The parameters for the different clusters
         mu1 = 1
@@ -17,27 +18,46 @@ class SimpleDataset:
 
         x1 = np.random.normal(mu1, sigma1, n)
         x2 = np.random.normal(mu2, sigma2, n)
-        self.x = np.concatenate(x1, x2)
+        self.x = np.concatenate([x1, x2])
 
         y1 = np.random.normal(mu1, sigma1, n)
         y2 = np.random.normal(mu2, sigma2, n)
-        self.y = np.concatenate(y1, y2)
+        self.y = np.concatenate([y1, y2])
 
+#FIXME: return a data frame instead of an object as in "class SimleDataset"
+'''
+def CreateDataFrame(numberOfPoints=100):
+    colNames = np.array(['index', 'x', 'y'])
+    indices = np.arange(numberOfPoints)
 
-#Number of generated data points
-n = 100
+    # FIXME: make this non-static so you can make high-dimensional data
+    # The parameters for the different clusters
+    mu1 = 1
+    mu2 = 4
+    sigma1 = 0.5
+    sigma2 = 1
 
-#The parameters for the different clusters
-mu1 = 1
-mu2 = 4
-sigma1 = 0.5
-sigma2 = 1
+    # Number of generated data points
+    labels1 = np.ones(numberOfPoints)
+    labels2 = 2*np.ones(numberOfPoints)
+    labels = np.concatenate([labels1, labels2])
 
-x1 = np.random.normal(mu1, sigma1, n)
-x2 = np.random.normal(mu2, sigma2, n)
+    x1 = np.random.normal(mu1, sigma1, numberOfPoints)
+    x2 = np.random.normal(mu2, sigma2, numberOfPoints)
+    x = np.concatenate([x1, x2])
 
-y1 = np.random.normal(mu1, sigma1, n)
-y2 = np.random.normal(mu2, sigma2, n)
+    y1 = np.random.normal(mu1, sigma1, numberOfPoints)
+    y2 = np.random.normal(mu2, sigma2, numberOfPoints)
+    y = np.concatenate([y1, y2])
+
+    data1 = np.array(['', 'x', 'y'],
+                     [indices, x1, y1])
+
+    #data1 = np.r_[colNames, data1]
+
+    return data1
+
+'''
 
 data = SimpleDataset(100)
 
